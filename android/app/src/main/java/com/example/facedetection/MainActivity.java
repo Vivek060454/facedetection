c2717d882c6c6efbe8388f7d8ca1e426236381eb
Vivek060454 <98967443+Vivek060454@users.noreply.github.com>
2023-02-20 11:53:37 +0530

    Add files via upload

---
+++ android/app/src/main/java/com/example/facedetection/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.facedetection;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/vision/detector.dart
@@ -0,0 +1,6 @@
+export 'barcode.dart';
+export 'digital.dart';
+export 'facedetector.dart';
+export 'label.dart';
+
+export 'test.dart';
